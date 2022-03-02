5f99ee74cd6adc66b709249c9bc38977fe490a74
Elian Batista <elianbatista2000@gmail.com>
2022-03-01 21:16:15 -0300

    feat: ensure Login navigates to signup correctly

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={null} />
-      </Routes>
+      <Switch>
+        <Route path="/login" exact component={null} />
+      </Switch>
     </BrowserRouter>
   )
 }
